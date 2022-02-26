be24a77ab5262a049aba31456b14b1c0d13a1edb
Kyle Franz <kmfranz@protonmail.com>
2022-02-26 12:42:57 -0800

    add some off chain stuff

--- src/Offchain/OrderActions.hs
+++ src/Offchain/OrderActions.hs
@@ -1,3 +1,40 @@
-module OrderActions where
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE NumericUnderscores    #-}
+
+module Offchain.OrderActions where
+
+import           Ledger
+import           Plutus.Contract
+
+data PlaceOrderParams = PlaceOrderParams {
+    pOwner     :: !PubKeyHash,
+    pBuyValue  :: !Value,
+    pSellValue :: !Value
+}
+
+data CancelOrderParams = CancelOrderParams {
+    cOwner     :: !PubKeyHash
+}
+
+data TakeOrderParams = TakeOrderParams {
+    tOwner     :: !PubKeyHash,
+    tBuyValue  :: !Value,
+    tSellValue :: !Value
+}
+
+type OrderActionSchema =
+            Endpoint "placeOrder"  PlaceOrderParams
+        .\/ Endpoint "cancelOrder" CancelOrderParams
+        .\/ Endpoint "takeOrder"   TakeOrderParams
 
 
