70e8bf5056c560997cdc5268dfa9d0b6aa12fbdb
Kyle Franz <kmfranz@protonmail.com>
2022-02-28 13:10:38 -0800

    add utxo filter template for building order

--- src/Offchain/OrderActions.hs
+++ src/Offchain/OrderActions.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE TypeFamilies        #-}
 {-# LANGUAGE TypeOperators       #-}
 {-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE DeriveAnyClass      #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE DeriveAnyClass        #-}
 {-# LANGUAGE NumericUnderscores    #-}
@@ -18,15 +19,19 @@ module Offchain.OrderActions (
     orderActionEndpoints
 ) where
 
-import           Control.Monad        hiding (fmap)
+import           Control.Monad          hiding (fmap)
 import           Data.Aeson             (ToJSON, FromJSON)
 import           Data.Text              (Text)
+import           Data.Map               as Map
 import           GHC.Generics           (Generic)
 import           Ledger
 import           Ledger.Ada             as Ada
 import qualified Ledger.Constraints     as Constraints
 import           Playground.Contract    (ToSchema)
 import           Plutus.Contract
+import           PlutusTx               (Data (..))
+import           PlutusTx
+import           PlutusTx
 import           PlutusTx.Prelude       hiding (Semigroup(..), unless)
 import           Prelude                (IO, Semigroup (..), Show (..), String)
 import           Text.Printf            (printf)
@@ -56,17 +61,18 @@ type OrderActionSchema =
 
 placeOrder :: AsContractError e => PlaceOrderParams -> Contract w s e ()
 placeOrder op = do
-    let dat = OrderDatum {
-            odOwner = pOwner op,
-            odBook = Ledger.PubKeyHash "c2ff616e11299d9094ce0a7eb5b7284b705147a822f4ffbd471f971a",
-            odBuyValue = pBuyValue op,
-            odSellValue = pSellValue op
-        }
-    let p = OrderParams { scriptVersion = "0.0.1" }
-        tx = Constraints.mustPayToTheScript dat $ (odSellValue dat)
-    ledgerTx <- submitTxConstraints ( typedValidator p ) tx
-    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
-    logInfo @String $ printf "place order endpoint"
+        let dat = OrderDatum {
+                odOwner = pOwner op,
+                odBook = Ledger.PubKeyHash "c2ff616e11299d9094ce0a7eb5b7284b705147a822f4ffbd471f971a",
+                odBuyValue = pBuyValue op,
+                odSellValue = pSellValue op
+            }
+        let p = OrderParams { scriptVersion = "0.0.1" }
+            tx = Constraints.mustPayToTheScript dat $ (odSellValue dat)
+        ledgerTx <- submitTxConstraints ( typedValidator p ) tx
+        void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+        logInfo @String $ printf "place order endpoint"
+
 
 cancelOrder :: AsContractError e => CancelOrderParams -> Contract w s e ()
 cancelOrder co = do
@@ -74,10 +80,17 @@ cancelOrder co = do
 
 takeOrder :: AsContractError e => TakeOrderParams -> Contract w s e ()
 takeOrder to = do
-    let bookAddress = Ledger.PubKeyHash "c2ff616e11299d9094ce0a7eb5b7284b705147a822f4ffbd471f971a"
-    let p = OrderParams { scriptVersion = "0.0.1" }
-    utxos <- utxosAt $ scrAddress p
-    logInfo @String $ printf "take order endpoint"
+        let bookAddress = Ledger.PubKeyHash "c2ff616e11299d9094ce0a7eb5b7284b705147a822f4ffbd471f971a"
+        let p = OrderParams { scriptVersion = "0.0.1" }
+        utxos <- Map.filter isSuitableUtxo <$> (utxosAt $ scrAddress p)
+        logInfo @String $ printf "take order endpoint"
+    where
+        isSuitableUtxo :: ChainIndexTxOut -> Bool
+        isSuitableUtxo o = case _ciTxOutDatum o of
+            Left _ -> False
+            Right (Datum e) -> case PlutusTx.fromBuiltinData e :: Maybe OrderDatum of
+                Nothing -> False
+                Just d -> True
 
 orderActionEndpoints :: Contract () OrderActionSchema Text ()
 orderActionEndpoints = awaitPromise (placeOrder' `select` cancelOrder' `select` takeOrder') >> orderActionEndpoints
