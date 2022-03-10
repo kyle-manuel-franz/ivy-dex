5203041a2ae41f1a801e86df0922eb18bb08a44b
Kyle Franz <kmfranz@protonmail.com>
2022-03-10 13:02:25 -0800

    add plutus file build

---
+++ src/Deploy/Order.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
+
+module Deploy.Order where
+
+import           Cardano.Api
+import           Cardano.Api.Shelley   (PlutusScript (..))
+import           Codec.Serialise       (serialise)
+import           Data.Aeson            (encode)
+import qualified Data.ByteString.Lazy  as LBS
+import qualified Data.ByteString.Short as SBS
+import           PlutusTx              (Data (..))
+import qualified PlutusTx
+import qualified Ledger
+
+import Contracts.Order
+
+dataToScriptData :: Data -> ScriptData
+dataToScriptData (Constr n xs) = ScriptDataConstructor n $ dataToScriptData <$> xs
+dataToScriptData (Map xs)      = ScriptDataMap [(dataToScriptData x, dataToScriptData y) | (x, y) <- xs]
+dataToScriptData (List xs)     = ScriptDataList $ dataToScriptData <$> xs
+dataToScriptData (I n)         = ScriptDataNumber n
+dataToScriptData (B bs)        = ScriptDataBytes bs
+
+writeValidator :: FilePath -> Ledger.Validator -> IO (Either (FileError ()) ())
+writeValidator file = writeFileTextEnvelope @(PlutusScript PlutusScriptV1) file Nothing . PlutusScriptSerialised . SBS.toShort . LBS.toStrict . serialise . Ledger.unValidatorScript
+
+writeVestingValidator :: Integer -> IO (Either (FileError ()) ())
+writeVestingValidator v = writeValidator ("dist/testnet/order_v_"++(show v)++".plutus") $ validator $ OrderParams
+    { scriptVersion = "0.0.1"
+    }
